7d06ee2d6188d47ff7afaa82af09a8ad1c0b3107
Nicolas Ayala <67024015+klitown@users.noreply.github.com>
2020-06-23 22:31:07 -0400

    Nueva sección
    Agregado la seccion de intereses personales

---
+++ scripts/slider.js
@@ -0,0 +1,7 @@
+const carouselSlide = document.querySelector(".carousel.slide"); /* seleccionar los slides */
+const caruselImages = document.querySelectorAll(".carousel-slide img") /* seleccionar las imagenes del carousel */ 
+
+// buttons
+
+const prev = document.querySelector("#prev");
+const next = document.querySelector("#next");
